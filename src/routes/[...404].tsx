8ab99296b387026c3a5b28f326727b22e35664d1
Kirato77 <justkirato@gmail.com>
2025-02-03 15:50:29 +0100

    rebase project with tailwindcss + init solid-ui

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,19 +1,9 @@
-import { Title } from "@solidjs/meta";
-import { HttpStatusCode } from "@solidjs/start";
+import { A } from "@solidjs/router";
 
 export default function NotFound() {
   return (
-    <main>
-      <Title>Not Found</Title>
-      <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Not Found</h1>
     </main>
   );
 }
