8f86b920823cbff9e0b7c75b24392e31a2aa91b2
Kirato77 <justkirato@gmail.com>
2025-02-04 10:19:45 +0100

    add assets and update navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,16 +3,29 @@ import { useLocation } from "@solidjs/router";
 export default function Nav() {
   const location = useLocation();
   const active = (path: string) =>
-    path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+    path == location.pathname ? "border-red-500" : "border-transparent hover:border-red-400";
   return (
-    <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-          <a href="/">Home</a>
+    <nav class="bg-gray-800">
+      <ul class="container flex justify-between w-full items-center p-3 text-gray-200">
+        <li>
+          <a href="/">
+            <div class="flex flex-row gap-2 items-center">
+              <img src="/src/assets/aleducation_logo.png" class="w-11 h11" />
+              <span class={`border-b-2 ${active("/")}`}><span class="text-red-500">ALED</span>UCATION</span>
+            </div>
+          </a>
         </li>
-        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <a href="/about">About</a>
-        </li>
+        <div class="flex flex-row">
+          <li class={`border-b-2 ${active("/formations")} mx-1.5 sm:mx-6 hover:text-red-400`}>
+            <a href="/formations">Nos Formations</a>
+          </li>
+          <li class={`border-b-2 border-transparent ${active("/formateurs")} mx-1.5 sm:mx-6 hover:text-red-400`}>
+            <a href="/formateurs">Nos Formateurs</a>
+          </li>
+          <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6 hover:text-red-400`}>
+            <a href="/about">Qui?</a>
+          </li>
+        </div>
       </ul>
     </nav>
   );
---
+++ src/routes/formations.tsx
@@ -0,0 +1,9 @@
+import { A } from "@solidjs/router";
+
+export default function Formations() {
+    return (
+        <main class="text-center mx-auto text-gray-700 p-4">
+            <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Nos Formations</h1>
+        </main>
+    );
+}
